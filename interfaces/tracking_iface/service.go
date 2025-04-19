b8cdfe16ed0244325165c7e4037ca633cc868cc6
vaziria <manorder123@gmail.com>
2025-04-19 11:56:00 +0700

    tambah tracking interface

--- interfaces/tracking_iface/service.go
+++ interfaces/tracking_iface/service.go
@@ -8,6 +8,22 @@ import (
 	"github.com/pdcgo/v2_gots_sdk/pdc_api"
 )
 
+type TrackErrCode string
+
+const (
+	InvalidReceipt TrackErrCode = "invalid_receipt"
+)
+
+type TrackError struct {
+	Code    TrackErrCode `json:"code"`
+	Receipt string       `json:"receipt"`
+}
+
+// Error implements error.
+func (t *TrackError) Error() string {
+	return fmt.Sprintf("[%s] %s", t.Code, t.Receipt)
+}
+
 type UnsupportedTrackErr struct {
 	ShippingID uint
 }
