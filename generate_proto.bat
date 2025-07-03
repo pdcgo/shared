4727fb1463e1baa8cb1edb99ded596b70a5ab10f
vaziria <manorder123@gmail.com>
2025-07-03 18:38:38 +0700

    adding protobuff implementation

--- generate_proto.bat
+++ generate_proto.bat
@@ -6,7 +6,7 @@ echo Script is running from: %SCRIPT_DIR%
 
 for /R proto %%F in (*.proto) do (
     echo Processing: %%F
-	protoc -I=proto --proto_path=%SCRIPT_DIR% --go_out=interfaces --go-grpc_out=interfaces %%F
+	protoc -I=proto --proto_path=%SCRIPT_DIR% --go_out=interfaces --go-grpc_out=interfaces --grpc-gateway_out=interfaces --openapiv2_out=interfaces %%F
 )
 
 echo Done.
