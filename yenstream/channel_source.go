263d76e7f27f18b10f9693942453add53a695992
vaziria <manorder123@gmail.com>
2025-07-12 11:43:39 +0700

    change channel source

--- yenstream/channel_source.go
+++ yenstream/channel_source.go
@@ -5,10 +5,17 @@ var _ Source = (*ChannelSource[any])(nil)
 type ChannelSource[T any] struct {
 	ctx *RunnerContext
 	out NodeOut
+	in  chan T
 }
 
 // Process implements Outlet.
-func (c *ChannelSource[T]) Process() {}
+func (c *ChannelSource[T]) Process() {
+	out := c.out.C()
+	defer close(out)
+	for d := range c.in {
+		out <- d
+	}
+}
 
 // Out implements Source.
 func (c *ChannelSource[T]) Out() NodeOut {
@@ -17,7 +24,7 @@ func (c *ChannelSource[T]) Out() NodeOut {
 
 func (c *ChannelSource[T]) Emit(data T) T {
 	go func() {
-		c.out.C() <- data
+		c.in <- data
 	}()
 	return data
 }
@@ -28,15 +35,16 @@ func (c *ChannelSource[T]) Via(label string, pipe Pipeline) Pipeline {
 	return pipe
 }
 
-func NewChannelSource[T any](ctx *RunnerContext) *ChannelSource[T] {
+func NewChannelSource[T any](ctx *RunnerContext, in chan T) *ChannelSource[T] {
+	if in == nil {
+		in = make(chan T, 1)
+	}
 	source := ChannelSource[T]{
 		out: NewNodeOut(ctx),
+		in:  in,
+		ctx: ctx,
 	}
 
-	go func() {
-		<-ctx.Done()
-		close(source.out.C())
-	}()
-
+	ctx.AddProcess(source.Process)
 	return &source
 }
