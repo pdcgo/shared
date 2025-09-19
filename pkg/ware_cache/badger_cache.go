0df3420c97714123d54d3465062273108746ce46
vaziria <manorder123@gmail.com>
2025-09-19 16:23:48 +0700

    badger test

---
+++ pkg/ware_cache/badger_cache.go
@@ -0,0 +1,105 @@
+package ware_cache
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/dgraph-io/badger/v4"
+)
+
+type BadgerCache struct {
+	db *badger.DB
+}
+
+// NewBadgerCache opens a BadgerDB instance at the given path.
+func NewBadgerCache(path string) (*BadgerCache, error) {
+	opts := badger.DefaultOptions(path).
+		WithLogger(nil) // silence internal logs, optional
+
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BadgerCache{db: db}, nil
+}
+
+func (b *BadgerCache) Close() error {
+	return b.db.Close()
+}
+
+// Add inserts a new item. If the key already exists, it returns an error.
+func (b *BadgerCache) Add(ctx context.Context, item *CacheItem) error {
+	data, err := item.Serialize()
+	if err != nil {
+		return err
+	}
+
+	return b.db.Update(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(item.Key))
+		if err == nil {
+			return errors.New("key already exists")
+		}
+		e := badger.NewEntry([]byte(item.Key), data)
+		if item.Expiration > 0 {
+			e = e.WithTTL(item.Expiration)
+		}
+		return txn.SetEntry(e)
+	})
+}
+
+// Flush deletes all keys (dangerous, use carefully).
+func (b *BadgerCache) Flush(ctx context.Context) error {
+	return b.db.DropAll()
+}
+
+func (b *BadgerCache) Get(ctx context.Context, key string, data any) error {
+	return b.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			if errors.Is(err, badger.ErrKeyNotFound) {
+				return ErrCacheMiss
+			}
+			return err
+		}
+		return item.Value(func(val []byte) error {
+			return json.Unmarshal(val, data)
+		})
+	})
+}
+
+func (b *BadgerCache) Replace(ctx context.Context, item *CacheItem) error {
+	data, err := item.Serialize()
+	if err != nil {
+		return err
+	}
+
+	return b.db.Update(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(item.Key))
+		if err != nil {
+			if errors.Is(err, badger.ErrKeyNotFound) {
+				return ErrCacheMiss
+			}
+			return err
+		}
+		e := badger.NewEntry([]byte(item.Key), data)
+		if item.Expiration > 0 {
+			e = e.WithTTL(item.Expiration)
+		}
+		return txn.SetEntry(e)
+	})
+}
+
+func (b *BadgerCache) Delete(ctx context.Context, key string) error {
+	return b.db.Update(func(txn *badger.Txn) error {
+		err := txn.Delete([]byte(key))
+		if err != nil {
+			if errors.Is(err, badger.ErrKeyNotFound) {
+				return ErrCacheMiss
+			}
+			return err
+		}
+		return nil
+	})
+}
---
+++ pkg/ware_cache/badger_test.go
@@ -0,0 +1,108 @@
+package ware_cache_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/pdcgo/shared/pkg/ware_cache"
+	"github.com/stretchr/testify/require"
+)
+
+func newTestCache(t *testing.T) *ware_cache.BadgerCache {
+	dir := filepath.Join(os.TempDir(), "badger_test")
+	_ = os.RemoveAll(dir) // cleanup before test
+
+	c, err := ware_cache.NewBadgerCache(dir)
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		c.Close()
+		os.RemoveAll(dir)
+	})
+
+	return c
+}
+
+func TestAddAndGet(t *testing.T) {
+	c := newTestCache(t)
+	ctx := context.Background()
+
+	item := &ware_cache.CacheItem{
+		Key:        "foo",
+		Expiration: time.Minute,
+		Data:       map[string]string{"msg": "hello"},
+	}
+	err := c.Add(ctx, item)
+	require.NoError(t, err)
+
+	var result map[string]string
+	err = c.Get(ctx, "foo", &result)
+	require.NoError(t, err)
+	require.Equal(t, "hello", result["msg"])
+
+	// Adding duplicate should fail
+	err = c.Add(ctx, item)
+	require.Error(t, err)
+}
+
+func TestReplace(t *testing.T) {
+	c := newTestCache(t)
+	ctx := context.Background()
+
+	// Replace on missing key should fail
+	item := &ware_cache.CacheItem{
+		Key:  "bar",
+		Data: "first",
+	}
+	err := c.Replace(ctx, item)
+	require.Error(t, err)
+
+	// Add first
+	require.NoError(t, c.Add(ctx, item))
+
+	// Replace should work
+	item.Data = "second"
+	require.NoError(t, c.Replace(ctx, item))
+
+	var result string
+	require.NoError(t, c.Get(ctx, "bar", &result))
+	require.Equal(t, "second", result)
+}
+
+func TestDelete(t *testing.T) {
+	c := newTestCache(t)
+	ctx := context.Background()
+
+	item := &ware_cache.CacheItem{
+		Key:  "baz",
+		Data: 123,
+	}
+	require.NoError(t, c.Add(ctx, item))
+
+	// Delete it
+	require.NoError(t, c.Delete(ctx, "baz"))
+
+	// Should not be found
+	var out int
+	err := c.Get(ctx, "baz", &out)
+	require.ErrorIs(t, err, ware_cache.ErrCacheMiss)
+}
+
+func TestFlush(t *testing.T) {
+	c := newTestCache(t)
+	ctx := context.Background()
+
+	require.NoError(t, c.Add(ctx, &ware_cache.CacheItem{
+		Key:  "flushme",
+		Data: true,
+	}))
+
+	require.NoError(t, c.Flush(ctx))
+
+	var out bool
+	err := c.Get(ctx, "flushme", &out)
+	require.ErrorIs(t, err, ware_cache.ErrCacheMiss)
+}
