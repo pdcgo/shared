5e8ca5f88bf2e873ad92fd5cb82b1f5b73b540a8
vaziria <manorder123@gmail.com>
2025-11-12 18:22:21 +0700

    adding config integration

--- configs/integration.go
+++ configs/integration.go
@@ -10,8 +10,9 @@ func (s *LegacyService) JoinPath(path string) string {
 }
 
 type AppProductionTestConfig struct {
-	JwtSecret         string            `yaml:"jwt_secret"`
-	Database          DatabaseConfig    `yaml:"database"`
-	LegacyService     LegacyService     `yaml:"legacy_service"`
-	AccountingService AccountingService `yaml:"accounting_service"`
+	JwtSecret             string            `yaml:"jwt_secret"`
+	Database              DatabaseConfig    `yaml:"database"`
+	SharedServiceEndpoint string            `yaml:"shared_service_endpoint"`
+	LegacyService         LegacyService     `yaml:"legacy_service"`
+	AccountingService     AccountingService `yaml:"accounting_service"`
 }
